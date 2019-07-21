f5c28baee2a61c2573cdd6feaeb3b6513ff65c2b
Ian Geckeler <ikcgeckeler@gmail.com>
2019-07-20 20:43:42 -0400

    package build works

--- examples/firecomm/server.js
+++ examples/firecomm/server.js
@@ -2,21 +2,15 @@ const grpc = require('grpc')
 
     const firecomm = require('../../index');
 
-// const package = require('./packageDefinition');
+const package = require('./packageDefinition');
 
-const package = require('./routeguide')
-
-    const {unaryChat, serverStream, clientStream, bidiChat} =
-        require('./methodHandlers');
+const {unaryChat, serverStream, clientStream, bidiChat} =
+    require('./methodHandlers');
 
 const server = new firecomm.Server();
 
 server.addService(
     package.RouteGuide, {unaryChat, serverStream, clientStream, bidiChat});
-
-// server.server.addService(
-//     package.RouteGuide.service,
-//     {unaryChat, serverStream, clientStream, bidiChat});
 
 
 server.bind('0.0.0.0:3000')
