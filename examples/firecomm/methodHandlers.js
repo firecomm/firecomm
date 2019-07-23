f70b782f9dbc4320e85a2758faa81f67f81aba6a
Ian Geckeler <ikcgeckeler@gmail.com>
2019-07-23 14:48:36 -0400

    integrated zeps changes and tweaked client and server streaming methods

--- examples/firecomm/methodHandlers.js
+++ examples/firecomm/methodHandlers.js
@@ -1,48 +1,24 @@
 const grpc = require('grpc');
 
 function unaryChat(ctx) {
-  ctx.setStatus({ 'trailer': 'value' });
+  ctx.setStatus({'trailer': 'value'});
   // ctx.throw(new Error('custom error message'));
   // console.log(ctx.req.meta);
   // console.log(ctx.req.data);
   // ctx.setTrailer({'hello': 'trailer'})
   // // ctx.throw({err: 'bad'})
   // ctx.setMeta({'hello': 'world'})
-  ctx.send({ message: 'what\'s up' })
-}
+  ctx.send({message: 'what\'s up'})
+  }
 
-// function unaryChat(call, callback) {
-//   const meta = new grpc.Metadata();
-//   const {request} = call;
-//   console.log(request);
-//   const response = {message: request.message + ' World'};
-//   callback(null, response, meta);
-// }
-
-function serverStream(call) {
-  const { request } = call;
-  call.write({ message: request.message + ' World' });
-}
-
-// function clientStream(call, callback) {
-//   // console.log(call.request);
-//   // console.log("is it callback:", callback);
-//   call.on('data', data => {
-//     console.log('data:', data);
-//   });
-//   call.on('end', () => {
-//     const trailer = new grpc.Metadata();
-//     const response = { message: ' World' };
-//     callback(null, response, trailer);
-//   });
-// }
+function serverStream(context) {
+  context.write({message: ' World'});
+};
 
 function clientStream(context) {
-  console.log('serverStream context: ', context)
-  context.on('data', data => {
-    console.log(data)
-  })
-}
+  console.log('serverStream context: ', context);
+  context.on('data', data => {console.log(data)});
+  }
 
 
 function bidiChat(context) {
@@ -52,7 +28,7 @@ function bidiChat(context) {
 
   context.on('data', data => {
     console.log('data:', data);
-    context.write({ message: data.message + ' World' });
+    context.write({message: data.message + ' World'});
   });
   context.throw(new Error('error'));
   setTimeout(() => {
