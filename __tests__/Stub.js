4b09f1d6e2f56935da1d8fed4ca80620cb2ad099
Ian Geckeler <ikcgeckeler@gmail.com>
2019-07-24 18:39:23 -0400

    successfully checks for class extension

--- __tests__/Stub.js
+++ __tests__/Stub.js
@@ -2,22 +2,77 @@ const {Stub} = require('../index');
 const grpc = require('grpc');
 
 
+// assign method... properly gets the method names...
+
+// successfully promisifies a unary
+
 
 describe('Unit tests for Stub', () => {
 
-  class mockServiceDef{
-    constructor(port, credentials) {
-      this.service = {key: 'key'};
-    }
-  }
 
-  it('Should extend a service definition',
+
+  it('Should extend a service definition', () => {
+    class mockServiceDef {
+      constructor(port, credentials) {}
+    }
+    mockServiceDef.service = {
+      HandlerName: {requestStream: true, responseStream: false}
+    };
+
+    const mockPort = '0.0.0.0:3000';
+    const stub = Stub(mockServiceDef, mockPort);
+    console.log(stub.constructor)
+    expect(stub instanceof mockServiceDef).toBeTruthy();
+  });
+
+  it('Adds a method to stub matching the method handler name on service definition.',
      () => {
-       console.log('mockServiceDef', mockServiceDef);
+       class mockServiceDef {
+         constructor(port, credentials) {
+           this.service = {
+             HandlerName: {requestStream: true, responseStream: false}
+           };
+         }
+         }
+
        const mockPort = '0.0.0.0:3000';
        const stub = Stub(mockServiceDef, mockPort);
-       expect(stub instanceof mockServiceDef).toBeTruthy()
+
+       console.log({stub})
+       expect(typeof stub.handlerName).toBe('function')
      })
+
+  it('Promisifies Unary.', () => {
+
+    class mockServiceDef {
+      constructor(port, credentials) {
+        this.service = {
+          HandlerName: {requestStream: false, responseStream: false}
+        };
+      }
+      }
+
+    const mockPort = '0.0.0.0:3000';
+    const stub = Stub(mockServiceDef, mockPort);
+
+    expect(stub.handlerName() instanceof Promise).toBeTruthy();
+
+  });
+
+  it('Wraps Duplex.',
+     () => {
+
+     });
+
+  it('Wraps ClientStream.',
+     () => {
+
+     });
+
+  it('Wraps ServerStream',
+     () => {
+
+     });
 
 
 })
