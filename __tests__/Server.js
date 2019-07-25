dece216f947b4f8b7d5196fc45dd9909385e895a
Ian Geckeler <ikcgeckeler@gmail.com>
2019-07-25 12:07:30 -0400

    added test for extension of server

--- __tests__/Server.js
+++ __tests__/Server.js
@@ -1,12 +1,38 @@
 const {Server} = require('../index');
 const grpc = require('grpc');
 
+// Mock the grpc server implementation
+
+const mockServer = jest.mock('./mocks/Server.js');
+
 describe('Unit tests for Server', () => {
-  it('Server Constructor throws an error when you pass in an object as a port.',
+
+  xit('Constructor extends the server class', () => {
+    const server = new Server();
+    expect(server instanceof grpc.Server).toBeTruthy();
+  })
+
+
+  it('Server bind throws an error when you pass in an object as a port.',
      () => {
        expect(() => {
          const server = new Server();
          server.bind({port: '0.0.0.0:3000'});
        }).toThrow();
      });
+  xit('Is an extension of the grpc server class.',
+      () => {
+
+      })
+
+  xit('Add service connects services to the server.',
+      () => {
+
+      })
+
+  xit('',
+      () => {
+
+      })
+
 });
