9566a92872b59055ed1411816618a3eec0ddc876
Ian Geckeler <ikcgeckeler@gmail.com>
2019-07-27 18:26:31 -0400

    removed unnecessary console.logs from Server tests

--- __tests__/Server.js
+++ __tests__/Server.js
@@ -20,19 +20,19 @@ var testProto = protoDescriptor.test;
 const testService = testProto.Test;
 
 describe("Unit tests for Server", () => {
-  xit("Constructor extends the grpc server class.", () => {
+  it("Constructor extends the grpc server class.", () => {
     const server = new Server();
     expect(server instanceof grpc.Server).toBeTruthy();
   });
 
-  xit("Server bind throws an error when you pass in an object as a port.", () => {
+  it("Server bind throws an error when you pass in an object as a port.", () => {
     expect(() => {
       const server = new Server();
       server.bind({ port: "0.0.0.0:3000" });
     }).toThrow();
   });
 
-  xit("addService composes and adds an async function.", () => {
+  it("addService composes and adds an async function.", () => {
     const server = new Server();
     server.addService(testService, {
       unaryCall: jest.fn(function() {})
@@ -42,7 +42,7 @@ describe("Unit tests for Server", () => {
     ).toBe("AsyncFunction");
   });
 
-  xit("addService throws an error when you add a handlername not included in the service.", () => {
+  it("addService throws an error when you add a handlername not included in the service.", () => {
     const server = new Server();
     expect(() => {
       server.addService(testService, {
@@ -51,7 +51,7 @@ describe("Unit tests for Server", () => {
     }).toThrow();
   });
 
-  xit("addService composes a service level middleware function that gets called when you call the handler.", () => {
+  it("addService composes a service level middleware function that gets called when you call the handler.", () => {
     const server = new Server();
     const mockMiddleware = jest.fn(function() {});
     server.addService(
@@ -62,28 +62,24 @@ describe("Unit tests for Server", () => {
       mockMiddleware
     );
     const fakeObject = {};
-    console.log(
-      server.handlers[Object.keys(server.handlers)[0]].func(fakeObject)
-    );
+    server.handlers[Object.keys(server.handlers)[0]].func(fakeObject);
     expect(mockMiddleware.mock.calls.length).toBe(1);
   });
 
-  xit("addService composes a method level middleware that is called on context object when handler is called.", () => {
+  it("addService composes a method level middleware that is called on context object when handler is called.", () => {
     const server = new Server();
     const mockMiddleware = jest.fn();
     server.addService(testService, {
       unaryCall: [mockMiddleware]
     });
     const fakeObject = {};
-    console.log(
-      server.handlers[Object.keys(server.handlers)[0]].func(fakeObject)
-    );
+    server.handlers[Object.keys(server.handlers)[0]].func(fakeObject);
     expect(mockMiddleware.mock.calls.length).toBe(1);
   });
 });
 
 describe("Uncaught Error Handling.", () => {
-  xit("Server level error handling should receive error and context object", () => {
+  it("Server level error handling should receive error and context object", () => {
     const mockErrorHandler = jest.fn();
     const server = new Server(mockErrorHandler);
     const mockMiddleware = jest.fn((err, call) => {
