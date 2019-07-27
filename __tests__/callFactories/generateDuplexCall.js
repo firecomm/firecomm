f64912d62b708ec2e89f84aca2b2b3709a24a1a9
dnaganog <dnaganog@gmail.com>
2019-07-26 20:05:54 -0400

    added unit tests for Duplex Call factory

--- __tests__/callFactories/generateDuplexCall.js
+++ __tests__/callFactories/generateDuplexCall.js
@@ -3,41 +3,18 @@ const mockServerReadableStream = {
   on: () => {},
   write: () => {},
 };
-const fakeCallback = (err, object, trailer) => {};
-const mockServerReadable = generateClientStreamCall(mockServerReadableStream, fakeCallback);
+const mockServerReadable = generateDuplexCall(mockServerReadableStream);
 
 describe('Unit test for generating Duplex Call', () => {
-  describe('Duplex should have properties', () => {
-
-    test('Duplex Call must have metaData property', () => {
-      expect(mockServerReadable.hasOwnProperty('metaData')).toBe(true);
-    })
-
-    test('Duplex Call must have err property', () => {
-      expect(mockServerReadable.hasOwnProperty('err')).toBe(true);
-    })
-
-    test('Duplex Call must have trailer property', () => {
-      expect(mockServerReadable.hasOwnProperty('trailer')).toBe(true);
-    })
-  });
-
+  
   describe('Duplex should have methods', () => {
 
     test('Duplex Call must have throw method', () => {
       expect(typeof mockServerReadable.throw === 'function').toBe(true);
     })
 
-    test('Duplex Call must have setStatus method', () => {
-      expect(typeof mockServerReadable.setStatus === 'function').toBe(true);
-    })
-
-    test('Duplex Call must have setMeta method', () => {
-      expect(typeof mockServerReadable.setMeta === 'function').toBe(true);
-    })
-
-    test('Duplex Call must have send method', () => {
-      expect(typeof mockServerReadable.send === 'function').toBe(true);
+    test('Duplex Call must have sendMeta method', () => {
+      expect(typeof mockServerReadable.sendMeta === 'function').toBe(true);
     })
 
     test('Duplex Call must have on method', () => {
