5d348d412324d31f99c683a5cc0d5164fd178d38
Ian Geckeler <ikcgeckeler@gmail.com>
2019-07-31 11:00:16 -0400

    added test for bind in server

--- examples/firecomm/server.js
+++ examples/firecomm/server.js
@@ -43,7 +43,7 @@ console.log({ server });
 // {private_key: (__dirname + '/server.crt'), certificate: (__dirname +
 // '/server.key')}
 server.bind("0.0.0.0:3000", {
-  before_private_key: __dirname + "/server.key",
+  privateKey: __dirname + "/server.key",
   certificate: __dirname + "/server.crt"
 });
 // console.log({server})
