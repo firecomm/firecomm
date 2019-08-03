0ac62a202405054b6dc0fe1286084d932eb95e70
Ian Geckeler <ikcgeckeler@gmail.com>
2019-08-03 12:00:38 -0400

    added client stream call tests

--- __tests__/clientCalls/clientStreamCall.js
+++ __tests__/clientCalls/clientStreamCall.js
@@ -1,4 +1,4 @@
-const duplexCall = require("../../lib/clientCalls/duplexCall");
+const clientStreamCall = require("../../lib/clientCalls/clientStreamCall");
 
 const mockMethod = jest.fn();
 const that = {};
@@ -8,22 +8,40 @@ beforeEach(() => {
   mockMethod.mockClear();
 });
 
-describe("Tests for duplexCall.", () => {
-  it("Test mock with no args", () => {
-    duplexCall(that, "methodName");
+describe("Tests for clientStreamCall.", () => {
+  it("Test mock with no args except for callback", () => {
+    clientStreamCall(that, "methodName", () => {});
     expect(mockMethod.mock.calls.length).toBe(1);
   });
 
   it("Interceptor array in second position gets called in the right place.", () => {
-    duplexCall(that, "methodName", { meta: "value" }, [() => {}]);
+    clientStreamCall(
+      that,
+      "methodName",
+      { meta: "value" },
+      [() => {}],
+      () => {}
+    );
     expect(typeof mockMethod.mock.calls[0][0]).toBe("object");
     expect(
       typeof mockMethod.mock.calls[0][1].hasOwnProperty("intercpetors")
     ).toBeTruthy();
   });
 
+  it("Throws when no callback is supplied.", () => {
+    expect(() => {
+      clientStreamCall(that, "methodName", { meta: "value" }, [() => {}]);
+    }).toThrow();
+  });
+
   it("Meta object in the second position gets called in the right place.", () => {
-    duplexCall(that, "methodName", [() => {}], { meta: "value" });
+    clientStreamCall(
+      that,
+      "methodName",
+      [() => {}],
+      { meta: "value" },
+      () => {}
+    );
     expect(typeof mockMethod.mock.calls[0][1]).toBe("object");
     expect(
       typeof mockMethod.mock.calls[0][0].hasOwnProperty("interceptors")
