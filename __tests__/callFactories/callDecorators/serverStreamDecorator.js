9ae443c0e0b6fed96bfb05312d24a7525ac33d7f
Ian Geckeler <ikcgeckeler@gmail.com>
2019-08-06 21:56:52 -0400

    laid down tests for server stream decorator

--- __tests__/callFactories/callDecorators/serverStreamDecorator.js
+++ __tests__/callFactories/callDecorators/serverStreamDecorator.js
@@ -1,23 +1,54 @@
-const object = {};
-const originalCall = {
-  hello: "hello"
-};
+const basicCallDecorator = require("../../../lib/callFactories/callDecorators/basicCallDecorator");
 const serverStreamDecorator = require("../../../lib/callFactories/callDecorators/serverStreamDecorator");
 
+const customCall = {};
+
+const mockEmit = jest.fn();
+const mockSendMetadata = jest.fn();
+const mockWrite = jest.fn();
+
+const originalCall = {
+  hello: "hello",
+  emit: mockEmit,
+  write: mockWrite,
+  sendMetadata: mockSendMetadata
+};
+
+basicCallDecorator(customCall, originalCall);
+serverStreamDecorator(customCall, originalCall);
+
 describe("server stream decorator tests.", () => {
+  beforeEach(() => {
+    mockWrite.mockClear();
+    mockEmit.mockClear();
+    mockSendMetadata.mockClear();
+  });
   describe("decorator adds the right methods", () => {
     //adds properties
-    beforeEach(() => {
-      serverStreamDecorator(object, originalCall);
-    });
+
     it("Has an set property", () => {
-      expect(object.hasOwnProperty("set")).toBeTruthy();
+      expect(customCall.hasOwnProperty("set")).toBeTruthy();
     });
     it("Has an send property", () => {
-      expect(object.hasOwnProperty("send")).toBeTruthy();
+      expect(customCall.hasOwnProperty("send")).toBeTruthy();
     });
     it("Has an throw property", () => {
-      expect(object.hasOwnProperty("throw")).toBeTruthy();
+      expect(customCall.hasOwnProperty("throw")).toBeTruthy();
     });
   });
+
+  describe("tests for throw", () => {
+    it("throw will throw if not an error", () => {
+      expect(() => customCall.throw("fake")).toThrow();
+    });
+    it("expect it to emit error", () => {
+      const fakeError = new Error("fake");
+      customCall.throw(fakeError);
+      expect(mockEmit.mock.calls[0][0]).toEqual("error");
+      expect(mockEmit.mock.calls[0][1]).toBe(fakeError);
+    });
+  });
+
+  describe("tests for set", () => {});
+  describe("tests for send", () => {});
 });
