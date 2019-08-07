d1a9a3c76b6a406b2e9a076fd7e475987887cdc3
Ian Geckeler <ikcgeckeler@gmail.com>
2019-08-06 21:47:37 -0400

    added tests for clientUnarydEcorator

--- __tests__/callFactories/callDecorators/clientUnaryDecorator.js
+++ __tests__/callFactories/callDecorators/clientUnaryDecorator.js
@@ -1,16 +1,46 @@
-const object = {};
-const originalCall = {
-  hello: "hello"
-};
 const clientUnaryDecorator = require("../../../lib/callFactories/callDecorators/clientUnaryDecorator");
+
+const customCall = {};
+
+const mockOn = jest.fn();
+const mockCallback = jest.fn();
+const originalCall = {
+  hello: "hello",
+  request: "request",
+  metadata: "metadata",
+  on: mockOn
+};
+
+clientUnaryDecorator(customCall, originalCall);
 
 describe("tests for client unary decorator", () => {
   describe("basic Tests for method assignment", () => {
-    beforeEach(() => {
-      clientUnaryDecorator(object, () => {});
-    });
     it("should have an on", () => {
-      expect(object.hasOwnProperty("on")).toBeTruthy();
+      expect(customCall.hasOwnProperty("on")).toBeTruthy();
+    });
+
+    it("should assign the body property to be call's request", () => {
+      expect(customCall.body).toBe(originalCall.request);
+    });
+
+    it("should assign the head to be the metadata", () => {
+      expect(customCall.head).toBe(originalCall.metadata);
+    });
+  });
+
+  describe("tests for on method", () => {
+    beforeEach(() => {
+      mockOn.mockClear();
+      mockCallback.mockClear();
+    });
+    it("on should accept a data string and callback", () => {
+      customCall.on("data", mockCallback);
+      expect(mockCallback.mock.calls.length).toBe(1);
+    });
+
+    it("should work without a string and just a callback", () => {
+      customCall.on(mockCallback);
+      expect(mockCallback.mock.calls.length).toBe(1);
     });
   });
 });
